60baa1b98ba474dd03c9c187ff8b5bb1a4beb220
Cory Kim <corykim0829@gmail.com>
2019-01-11 23:31:06 +0900

    ui: stackView layout

--- Tinder/ViewController.swift
+++ Tinder/ViewController.swift
@@ -12,9 +12,42 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        let grayView = UIView()
+        grayView.backgroundColor = .gray
+        
+        let subviews = [UIColor.gray, UIColor.darkGray, UIColor.black].map
+        { (color) -> UIView in
+            let v = UIView()
+            v.backgroundColor = color
+            return v
+        }
+        
+        let topStackView = UIStackView(arrangedSubviews: subviews)
+        topStackView.axis = .horizontal
+        topStackView.distribution = .fillEqually
+        topStackView.heightAnchor.constraint(equalToConstant: 100).isActive = true
+        let blueView = UIView()
+        blueView.backgroundColor = .blue
+        let yellowView = UIView()
+        yellowView.backgroundColor = .yellow
+        yellowView.heightAnchor.constraint(equalToConstant: 120).isActive = true
+        
+        let stackView = UIStackView(arrangedSubviews: [topStackView, blueView, yellowView])
+//        stackView.distribution = .fillEqually
+        stackView.axis = .vertical
+        
+        view.addSubview(stackView)
+        stackView.frame = .init(x: 0, y: 0, width: 300, height: 200)
+        
+        stackView.fillSuperview()
+        
+        // this enables auto layout for us
+//        stackView.translatesAutoresizingMaskIntoConstraints = false
+//        stackView.topAnchor.constraint(equalTo: view.topAnchor).isActive = true
+//        stackView.bottomAnchor.constraint(equalTo: view.bottomAnchor).isActive = true
+//        stackView.leadingAnchor.constraint(equalTo: view.leadingAnchor).isActive = true
+//        stackView.trailingAnchor.constraint(equalTo: view.trailingAnchor).isActive = true
     }
-
-
 }
 
