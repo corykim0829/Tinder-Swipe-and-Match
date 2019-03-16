07dc6fa4aa7134c4d229b20754a1553125369576
corykim0829 <corykim0829@gmail.com>
2019-03-16 21:12:35 +0900

    feat : handle function to registration Controller

---
+++ Tinder/Controllers/RegistrationController.swift
@@ -0,0 +1,19 @@
+//
+//  RegistrationController.swift
+//  Tinder
+//
+//  Created by Cory Kim on 16/03/2019.
+//  Copyright © 2019 CoryKim. All rights reserved.
+//
+
+import UIKit
+
+class RegistrationController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        view.backgroundColor = .red
+    }
+
+}
