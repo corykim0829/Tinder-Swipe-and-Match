627b0c96537a56ebf69a80344a4fecd4c58102dc
corykim0829 <corykim0829@gmail.com>
2019-03-17 00:59:25 +0900

    ui: for registrationController, CustomTextField

--- Tinder/Controllers/RegistrationController.swift
+++ Tinder/Controllers/RegistrationController.swift
@@ -9,11 +9,56 @@
 import UIKit
 
 class RegistrationController: UIViewController {
-
+    
+    // UI Components
+    let selectPhotoButton: UIButton = {
+        let button = UIButton(type: .system)
+        button.setTitle("Select Photo", for: .normal)
+        button.titleLabel?.font = UIFont.systemFont(ofSize: 32, weight: .heavy)
+        button.setTitleColor(.black, for: .normal)
+        button.backgroundColor = .white
+        button.layer.cornerRadius = 15
+        button.heightAnchor.constraint(equalToConstant: 275).isActive = true
+        return button
+    }()
+    
+    let fullNameTextField: CustomTextField = {
+        let tf = CustomTextField(padding: 24)
+        tf.placeholder = "Enter full name"
+        return tf
+    }()
+    
+    let emailTextField: CustomTextField = {
+        let tf = CustomTextField(padding: 24)
+        tf.placeholder = "Enter email"
+        tf.keyboardType = .emailAddress
+        return tf
+    }()
+    
+    let passwordTextField: CustomTextField = {
+        let tf = CustomTextField(padding: 24)
+        tf.placeholder = "Enter password"
+        tf.isSecureTextEntry = true
+        return tf
+    }()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
         view.backgroundColor = .red
+        
+        let stackView = UIStackView(arrangedSubviews: [
+            selectPhotoButton,
+            fullNameTextField,
+            emailTextField,
+            passwordTextField
+            ])
+        
+        view.addSubview(stackView)
+        stackView.spacing = 8
+        stackView.axis = .vertical
+        stackView.anchor(top: nil, leading: view.leadingAnchor, bottom: nil, trailing: view.trailingAnchor, padding: .init(top: 0, left: 50, bottom: 0, right: 50))
+        stackView.centerYAnchor.constraint(equalTo: view.centerYAnchor).isActive = true
     }
 
 }
---
+++ Tinder/Views/CustomTextField.swift
@@ -0,0 +1,38 @@
+//
+//  CustomTextField.swift
+//  Tinder
+//
+//  Created by Cory Kim on 17/03/2019.
+//  Copyright © 2019 CoryKim. All rights reserved.
+//
+
+import UIKit
+
+class CustomTextField: UITextField {
+    
+    let padding: CGFloat
+    
+    init(padding: CGFloat) {
+        self.padding = padding
+        super.init(frame: .zero)
+        self.backgroundColor = .white
+        self.layer.cornerRadius = 25
+    }
+    
+    override func textRect(forBounds bounds: CGRect) -> CGRect {
+        return bounds.insetBy(dx: padding, dy: 0)
+    }
+    
+    override func editingRect(forBounds bounds: CGRect) -> CGRect {
+        return bounds.insetBy(dx: padding, dy: 0)
+    }
+    
+    override var intrinsicContentSize: CGSize {
+        return .init(width: 0, height: 50)
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+}
+
