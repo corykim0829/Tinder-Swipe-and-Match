a257104273428b62b31f7a1fe2e18e223aa547a4
corykim0829 <corykim0829@gmail.com>
2019-03-30 23:37:39 +0900

    ui: settings navigation controller with header section

--- Tinder/Controllers/HomeController.swift
+++ Tinder/Controllers/HomeController.swift
@@ -70,9 +70,9 @@ class HomeController: UIViewController {
     }
     
     @objc func handleSettings() {
-        print("Show registration page")
-        let registrationController = RegistrationController()
-        present(registrationController, animated: true)
+        let settingsController = SettingsController()
+        let navController = UINavigationController(rootViewController: settingsController)
+        present(navController, animated: true)
     }
     
     // MARK:- Fileprivate
---
+++ Tinder/Controllers/SettingsController.swift
@@ -0,0 +1,78 @@
+//
+//  SettingsController.swift
+//  Tinder
+//
+//  Created by Cory Kim on 29/03/2019.
+//  Copyright © 2019 CoryKim. All rights reserved.
+//
+
+import UIKit
+
+class SettingsController: UITableViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
+    
+    // instance properties
+    lazy var image1Button = createButton(selector: #selector(handleSelectPhoto))
+    lazy var image2Button = createButton(selector: #selector(handleSelectPhoto))
+    lazy var image3Button = createButton(selector: #selector(handleSelectPhoto))
+    
+    @objc fileprivate func handleSelectPhoto() {
+        // imagePicker
+        let imagePicker = UIImagePickerController()
+        imagePicker.delegate = self
+        present(imagePicker, animated: true)
+    }
+    
+    func createButton(selector: Selector) -> UIButton {
+        let button = UIButton(type: .system)
+        button.setTitle("select photo", for: .normal)
+        button.addTarget(self, action: selector, for: .touchUpInside)
+        return button
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        setupNavigationItems()
+        tableView.backgroundColor = UIColor(white: 0.95, alpha: 1)
+        tableView.tableFooterView = UIView()
+    }
+    
+    override func tableView(_ tableView: UITableView, viewForHeaderInSection section: Int) -> UIView? {
+        let header = UIView()
+        header.addSubview(image1Button)
+        let padding: CGFloat = 16
+        image1Button.anchor(top: header.topAnchor, leading: header.leadingAnchor, bottom: header.bottomAnchor, trailing: nil, padding: UIEdgeInsets.init(top: padding, left: padding, bottom: padding, right: 0))
+        image1Button.widthAnchor.constraint(equalTo: header.widthAnchor, multiplier: 0.45).isActive = true
+        image1Button.backgroundColor = .white
+        
+        let stackView = UIStackView(arrangedSubviews: [image2Button, image3Button])
+        stackView.axis = .vertical
+        stackView.distribution = .fillEqually
+        stackView.spacing = padding
+        
+        header.addSubview(stackView)
+        stackView.anchor(top: header.topAnchor, leading: image1Button.trailingAnchor, bottom: header.bottomAnchor, trailing: header.trailingAnchor, padding: .init(top: padding, left: padding, bottom: padding, right: padding))
+        image2Button.backgroundColor = .white
+        image3Button.backgroundColor = .white
+        
+        return header
+    }
+    
+    override func tableView(_ tableView: UITableView, heightForHeaderInSection section: Int) -> CGFloat {
+        return 300
+    }
+    
+    fileprivate func setupNavigationItems() {
+        navigationItem.title = "Settings"
+        navigationController?.navigationBar.prefersLargeTitles = true
+        navigationItem.leftBarButtonItem = UIBarButtonItem(title: "Cancel", style: .plain, target: self, action: #selector(handleCancel))
+        navigationItem.rightBarButtonItems = [
+            UIBarButtonItem(title: "Save", style: .plain, target: self, action: #selector(handleCancel)),
+            UIBarButtonItem(title: "Logout", style: .plain, target: self, action: #selector(handleCancel))
+        ]
+    }
+    
+    @objc fileprivate func handleCancel() {
+        dismiss(animated: true)
+    }
+}
