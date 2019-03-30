da6425598a55dd7706b4477ca198123b9b1a9d6f
corykim0829 <corykim0829@gmail.com>
2019-03-30 23:48:44 +0900

    feat: select photo buttons with UIImagePickerController

--- Tinder/Controllers/SettingsController.swift
+++ Tinder/Controllers/SettingsController.swift
@@ -8,6 +8,11 @@
 
 import UIKit
 
+class CustomImagePickerController: UIImagePickerController {
+    var imageButton: UIButton?
+    
+}
+
 class SettingsController: UITableViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
     
     // instance properties
@@ -15,17 +20,31 @@ class SettingsController: UITableViewController, UIImagePickerControllerDelegate
     lazy var image2Button = createButton(selector: #selector(handleSelectPhoto))
     lazy var image3Button = createButton(selector: #selector(handleSelectPhoto))
     
-    @objc fileprivate func handleSelectPhoto() {
+    @objc fileprivate func handleSelectPhoto(button: UIButton) {
         // imagePicker
-        let imagePicker = UIImagePickerController()
+        let imagePicker = CustomImagePickerController()
         imagePicker.delegate = self
+        imagePicker.imageButton = button
         present(imagePicker, animated: true)
+    }
+    
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
+        
+        let selectedImage = info[.originalImage] as? UIImage
+        // how do I set the image on my buttons when I select a photo?
+        let imageButton = (picker as? CustomImagePickerController)?.imageButton
+        imageButton?.setImage(selectedImage?.withRenderingMode(.alwaysOriginal), for: .normal)
+        dismiss(animated: true)
     }
     
     func createButton(selector: Selector) -> UIButton {
         let button = UIButton(type: .system)
         button.setTitle("select photo", for: .normal)
         button.addTarget(self, action: selector, for: .touchUpInside)
+        button.backgroundColor = .white
+        button.imageView?.contentMode = .scaleAspectFill
+        button.layer.cornerRadius = 9
+        button.clipsToBounds = true
         return button
     }
     
@@ -43,7 +62,6 @@ class SettingsController: UITableViewController, UIImagePickerControllerDelegate
         let padding: CGFloat = 16
         image1Button.anchor(top: header.topAnchor, leading: header.leadingAnchor, bottom: header.bottomAnchor, trailing: nil, padding: UIEdgeInsets.init(top: padding, left: padding, bottom: padding, right: 0))
         image1Button.widthAnchor.constraint(equalTo: header.widthAnchor, multiplier: 0.45).isActive = true
-        image1Button.backgroundColor = .white
         
         let stackView = UIStackView(arrangedSubviews: [image2Button, image3Button])
         stackView.axis = .vertical
@@ -52,8 +70,6 @@ class SettingsController: UITableViewController, UIImagePickerControllerDelegate
         
         header.addSubview(stackView)
         stackView.anchor(top: header.topAnchor, leading: image1Button.trailingAnchor, bottom: header.bottomAnchor, trailing: header.trailingAnchor, padding: .init(top: padding, left: padding, bottom: padding, right: padding))
-        image2Button.backgroundColor = .white
-        image3Button.backgroundColor = .white
         
         return header
     }
