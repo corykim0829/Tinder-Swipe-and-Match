87ddc88c6aaf8b578d11e9ea9e57adc24823e921
corykim0829 <corykim0829@gmail.com>
2019-03-31 03:27:01 +0900

    ui: headers, rows in sections of tableView

--- Tinder/Controllers/SettingsController.swift
+++ Tinder/Controllers/SettingsController.swift
@@ -10,7 +10,6 @@ import UIKit
 
 class CustomImagePickerController: UIImagePickerController {
     var imageButton: UIButton?
-    
 }
 
 class SettingsController: UITableViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
@@ -52,11 +51,21 @@ class SettingsController: UITableViewController, UIImagePickerControllerDelegate
         super.viewDidLoad()
         
         setupNavigationItems()
+        setupTapGesture()
         tableView.backgroundColor = UIColor(white: 0.95, alpha: 1)
         tableView.tableFooterView = UIView()
+        tableView.keyboardDismissMode = .interactive
     }
     
-    override func tableView(_ tableView: UITableView, viewForHeaderInSection section: Int) -> UIView? {
+    fileprivate func setupTapGesture() {
+        tableView.addGestureRecognizer(UITapGestureRecognizer(target: self, action: #selector(handleTapDismiss)))
+    }
+    
+    @objc fileprivate func handleTapDismiss() {
+        tableView.endEditing(true)
+    }
+    
+    lazy var header: UIView = {
         let header = UIView()
         header.addSubview(image1Button)
         let padding: CGFloat = 16
@@ -70,13 +79,67 @@ class SettingsController: UITableViewController, UIImagePickerControllerDelegate
         
         header.addSubview(stackView)
         stackView.anchor(top: header.topAnchor, leading: image1Button.trailingAnchor, bottom: header.bottomAnchor, trailing: header.trailingAnchor, padding: .init(top: padding, left: padding, bottom: padding, right: padding))
-        
         return header
+    }()
+    
+    class HeaderLabel: UILabel {
+        override func drawText(in rect: CGRect) {
+            super.drawText(in: rect.insetBy(dx: 16, dy: 0))
+        }
+    }
+    
+    override func tableView(_ tableView: UITableView, viewForHeaderInSection section: Int) -> UIView? {
+        if section == 0 {
+            return header
+        }
+        
+        let headerLabel = HeaderLabel()
+        switch section {
+        case 1:
+            headerLabel.text = "Name"
+        case 2:
+            headerLabel.text = "Profession"
+        case 3:
+            headerLabel.text = "Age"
+        default:
+            headerLabel.text = "Bio"
+        }
+        headerLabel.font = UIFont.boldSystemFont(ofSize: headerLabel.font.pointSize)
+        return headerLabel
     }
     
     override func tableView(_ tableView: UITableView, heightForHeaderInSection section: Int) -> CGFloat {
-        return 300
+        
+        if section == 0 {
+            return 300
+        }
+        return 40
     }
+    
+    override func numberOfSections(in tableView: UITableView) -> Int {
+        return 5
+    }
+    
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return section == 0 ? 0 : 1
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = SettingsCell(style: .default, reuseIdentifier: nil)
+        
+        switch indexPath.section {
+        case 1:
+            cell.textField.placeholder = "Enter Name"
+        case 2:
+            cell.textField.placeholder = "Enter Profession"
+        case 3:
+            cell.textField.placeholder = "Enter Age"
+        default:
+            cell.textField.placeholder = "Enter Your Bio"
+        }
+        return cell
+    }
+    
     
     fileprivate func setupNavigationItems() {
         navigationItem.title = "Settings"
---
+++ Tinder/Views/SettingsCell.swift
@@ -0,0 +1,45 @@
+//
+//  SettingsCell.swift
+//  Tinder
+//
+//  Created by Cory Kim on 31/03/2019.
+//  Copyright © 2019 CoryKim. All rights reserved.
+//
+
+import UIKit
+
+class SettingsCell: UITableViewCell {
+    
+    class SettingsTextField: UITextField {
+        
+        override func textRect(forBounds bounds: CGRect) -> CGRect {
+            return bounds.insetBy(dx: 24, dy: 0)
+        }
+        
+        override func editingRect(forBounds bounds: CGRect) -> CGRect {
+            return bounds.insetBy(dx: 24, dy: 0)
+        }
+        
+        override var intrinsicContentSize: CGSize {
+            return .init(width: 0, height: 44)
+        }
+    }
+    
+    let textField: UITextField = {
+        let tf = SettingsTextField()
+        tf.placeholder = "Enter Name"
+        return tf
+    }()
+    
+    override init(style: UITableViewCell.CellStyle, reuseIdentifier: String?) {
+        super.init(style: style, reuseIdentifier: reuseIdentifier)
+        
+        addSubview(textField)
+        textField.fillSuperview()
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        fatalError()
+    }
+
+}
