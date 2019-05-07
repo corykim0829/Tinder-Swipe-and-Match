75582e1a90a24441164a61ed89f5a81d5e24f2fb
corykim0829 <corykim0829@gmail.com>
2019-05-08 00:19:46 +0900

    feat: visual blur effect view and layouts

--- Tinder/Controllers/User Details/UserDetailController.swift
+++ Tinder/Controllers/User Details/UserDetailController.swift
@@ -43,12 +43,69 @@ class UserDetailController: UIViewController, UIScrollViewDelegate {
         label.numberOfLines = 0
         return label
     }()
-
+    
+    let dismissButton: UIButton = {
+        let button = UIButton(type: .system)
+        button.setImage(#imageLiteral(resourceName: "dismiss_icon").withRenderingMode(.alwaysOriginal), for: .normal)
+        button.addTarget(self, action: #selector(handleDismiss), for: .touchUpInside)
+        return button
+    }()
+    
+    // 3 bottom control buttons
+    
+    lazy var dislikeButton = self.createButton(image: #imageLiteral(resourceName: "dismiss_circle"), selector: #selector(handleDislike))
+    lazy var superLikeButton = self.createButton(image: #imageLiteral(resourceName: "super_like_circle"), selector: #selector(handleSuperLike))
+    lazy var likeButton = self.createButton(image: #imageLiteral(resourceName: "like_circle"), selector: #selector(handleLike))
+    
+    @objc fileprivate func handleDislike() {
+        
+    }
+    
+    @objc fileprivate func handleSuperLike() {
+        
+    }
+    
+    @objc fileprivate func handleLike() {
+        
+    }
+    
+    fileprivate func createButton(image: UIImage, selector: Selector) -> UIButton {
+        let button = UIButton(type: .system)
+        button.setImage(image.withRenderingMode(.alwaysOriginal), for: .normal)
+        button.addTarget(self, action: selector, for: .touchUpInside)
+        button.imageView?.contentMode = .scaleAspectFill
+        return button
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
         view.backgroundColor = .white
         
+        setupLayout()
+        setupViusalBlurEffectView()
+        setupButtomControls()
+    }
+    
+    fileprivate func setupButtomControls() {
+        let stackView = UIStackView(arrangedSubviews: [dislikeButton, superLikeButton, likeButton])
+        stackView.axis = .horizontal
+        stackView.spacing = -32
+        stackView.distribution = .fillEqually
+        view.addSubview(stackView)
+        stackView.anchor(top: nil, leading: nil, bottom: view.safeAreaLayoutGuide.bottomAnchor, trailing: nil, size: .init(width: 300, height: 80))
+        stackView.centerXAnchor.constraint(equalTo: view.centerXAnchor).isActive = true
+    }
+    
+    fileprivate func setupViusalBlurEffectView() {
+        let blurEffect = UIBlurEffect(style: .regular)
+        let visualEffectView = UIVisualEffectView(effect: blurEffect)
+        
+        view.addSubview(visualEffectView)
+        visualEffectView.anchor(top: view.topAnchor, leading: view.leadingAnchor, bottom: view.safeAreaLayoutGuide.topAnchor, trailing: view.trailingAnchor)
+    }
+    
+    fileprivate func setupLayout() {
         view.addSubview(scrollView)
         scrollView.fillSuperview()
         
@@ -59,18 +116,19 @@ class UserDetailController: UIViewController, UIScrollViewDelegate {
         scrollView.addSubview(infoLabel)
         infoLabel.anchor(top: imageView.bottomAnchor, leading: scrollView.leadingAnchor, bottom: nil, trailing: scrollView.trailingAnchor, padding: .init(top: 16, left: 16, bottom: 0, right: 0))
         
-        view.addGestureRecognizer(UITapGestureRecognizer(target: self, action: #selector(handleTapDismiss)))
+        scrollView.addSubview(dismissButton)
+        dismissButton.anchor(top: imageView.bottomAnchor, leading: nil, bottom: nil, trailing: view.trailingAnchor, padding: .init(top: -25, left: 0, bottom: 0, right: 25), size: .init(width: 50, height: 50))
     }
     
     func scrollViewDidScroll(_ scrollView: UIScrollView) {
         let changeY = -scrollView.contentOffset.y
         var width = view.frame.width + changeY * 2
-        print(-changeY)
+//        print(-changeY)
         width = max(view.frame.width, width)
         imageView.frame = CGRect(x: min(0, -changeY), y: min(0, -changeY), width: width, height: width)
     }
     
-    @objc fileprivate func handleTapDismiss() {
+    @objc fileprivate func handleDismiss() {
         self.dismiss(animated: true)
     }
 
