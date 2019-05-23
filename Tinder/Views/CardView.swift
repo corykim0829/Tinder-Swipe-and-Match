a3c3477aaa9edca0c7419ed1a668a71dcb68ba73
corykim0829 <corykim0829@gmail.com>
2019-05-23 16:45:20 +0900

    feat: like button
    remove top cardView using linked list

--- Tinder/Views/CardView.swift
+++ Tinder/Views/CardView.swift
@@ -11,9 +11,12 @@ import SDWebImage
 
 protocol CardViewDelegate {
     func didTapMoreInfo(cardViewModel: CardViewModel)
+    func didRemoveCard(cardView: CardView)
 }
 
 class CardView: UIView {
+    
+    var nextCardView: CardView?
     
     var delegate: CardViewDelegate?
     
@@ -179,6 +182,7 @@ class CardView: UIView {
             self.transform = .identity
             if shouldDismissCard {
                 self.removeFromSuperview()
+                self.delegate?.didRemoveCard(cardView: self)
             }
 //            self.frame = CGRect(x: 0, y: 0, width: self.superview!.frame.width, height: self.superview!.frame.height)
         }
